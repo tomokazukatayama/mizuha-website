e7116f445bb0757e1c6fe3fc3fff2a9888366ab8
Yayoi Mizuha <basilia.gamemaker@gmail.com>
2019-11-20 02:06:29 +0900

    manually update

--- autodev.sh
+++ autodev.sh
@@ -3,8 +3,12 @@ code .
 while :
 do
 	hugo server
+	echo ""
+	echo ""
+	echo "Starting Deploy"
 	hugo --minify
 	firebase deploy
+	echo "Saving Changes to GitHub"
 	git add -A
 	git commit -m "manually update"
 	git push
